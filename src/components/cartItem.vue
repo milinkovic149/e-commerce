4b2daeac1f1192d5dd2e28e766ab6f1eafd1b3d6
milinkovic149 <marko.milinkovic@tms-outsource.com>
2023-09-22 13:58:57 +0200

    Update

--- src/components/cartItem.vue
+++ src/components/cartItem.vue
@@ -12,7 +12,7 @@ defineEmits(["updateCount", "clear"]);
 <template>
   <div class="item">
     <div class="item__products">
-      <img :src="product.img" alt="" class="item__products__product-img">
+      <img :src="product.img" alt="" class="item__products__product-img" width="373" height="373">
       <span>{{ product.name }}</span>
     </div>
 
